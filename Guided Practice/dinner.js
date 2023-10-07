56bd66beb6354fe2a074a449dfa9936598f5b597
blandero <landerosb08@gmail.com>
2023-10-07 12:42:11 -0600

    finished exercise

--- Guided Practice/dinner.js
+++ Guided Practice/dinner.js
@@ -6,3 +6,20 @@ const dinner = {
     soupAndSalad: 16
 };
 console.log(Object.values(dinner));
+
+const prices = Object.values(dinner);
+let totalCost = 0;
+
+for (let i = 0; i < prices.length; i++) {
+    totalCost += prices[i];
+}
+
+console.log(totalCost);
+
+function calculateCost(meal){
+    let total = 0;
+    for (const food in meal){
+        total += meal[food];
+    }
+    return total;
+}
